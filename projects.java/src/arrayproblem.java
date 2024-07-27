f8add76d380f55c11e5582f8bf31a2220662c185
Your Name <you@example.com>
2024-07-27 19:08:18 +0530

    Array basic problem

---
+++ projects.java/src/arrayproblem.java
@@ -0,0 +1,10 @@
+public class arrayproblem {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        System.out.println("Original array: ");
+        for (int x : arr) {
+            System.out.print(x);
+        }
+
+    }
+}
