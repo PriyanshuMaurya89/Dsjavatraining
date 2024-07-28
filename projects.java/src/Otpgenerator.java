e6da4737997b304ea6311d3722598a1e272f2c60
Your Name <you@example.com>
2024-07-29 04:06:03 +0530

    bubbls sort using java

--- projects.java/src/Otpgenerator.java
+++ projects.java/src/Otpgenerator.java
@@ -7,6 +7,6 @@ public class Otpgenerator {
         //to generate 6 digit otp random class used
         Random random = new Random();
         int myOtp = random.nextInt(1000000); // 6 times 0
-        System.out.println("Your Java OTP is: "+ myOtp+" don't share to anyone");
+        System.out.println("Your Java OTP is: " + myOtp + " don't share to anyone");
     }
 }
